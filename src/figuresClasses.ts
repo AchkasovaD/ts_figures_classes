61183b541ad0217f4aced5b2a9ee7dddcfb2d140
Daria Achkasova <daria.achkasova6@gmail.com>
2026-02-05 19:22:50 +0100

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,73 @@
-export interface Figure {}
+type Shape = 'triangle' | 'circle' | 'rectangle';
+type Color = 'red' | 'green' | 'blue';
 
-export class Triangle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
+  getArea(): number;
+}
 
-export class Circle implements Figure {}
+export class Triangle implements Figure {
+  constructor(
+    public color: Color,
+    public a = 0,
+    public b = 0,
+    public c = 0,
+    public shape: Shape = 'triangle',
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error('the length of side is 0 or negative');
+    }
 
-export class Rectangle implements Figure {}
+    const maximal = Math.max(a, b, c);
 
-export function getInfo(figure): string {
-  return typeof figure;
+    if (maximal >= a + b + c - maximal) {
+      throw new Error('wrong length');
+    }
+  }
+
+  getArea(): number {
+    const s = 0.5 * (this.a + this.b + this.c);
+
+    return +Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)).toFixed(
+      2,
+    );
+  }
+}
+
+export class Circle implements Figure {
+  constructor(
+    public color: Color,
+    public r = 0,
+    public shape: Shape = 'circle',
+  ) {
+    if (r <= 0) {
+      throw new Error('the length of radius is 0 or negative');
+    }
+  }
+
+  getArea(): number {
+    return Math.floor(Math.PI * this.r * this.r * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  constructor(
+    public color: Color,
+    public a = 0,
+    public b = 0,
+    public shape: Shape = 'rectangle',
+  ) {
+    if (a <= 0 || b <= 0) {
+      throw new Error('the length of side is 0 or negative');
+    }
+  }
+
+  getArea(): number {
+    return this.a * this.b;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
